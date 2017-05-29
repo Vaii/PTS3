50f74e2f4493236559f82c7f85433754e575e337
Wei-qiang Zhou <wei-qiang@hotmail.nl>
2017-05-29 11:41:49 +0200

    Dropbox (#24)
    * added storage settings
    
    * deleted dropbox library

---
+++ src/storage/StorageSettingsController.java
@@ -0,0 +1,19 @@
+package storage;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+/**
+ * Created by wei-qiang on 29-May-17.
+ */
+public class StorageSettingsController {
+    @FXML
+    private Button btnAuthorize;
+    @FXML
+    private Button btnSendToken;
+    @FXML
+    private Button btnOntkoppel;
+    @FXML
+    private TextArea tbToken;
+}
