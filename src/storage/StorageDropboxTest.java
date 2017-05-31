da1353785bdb751a4e9e6e8e38b86f8fe3000f52
Wei-qiang Zhou <wei-qiang@hotmail.nl>
2017-05-31 10:22:11 +0200

    2 tests teogevoegd voor storagedropbox (#41)

---
+++ src/storage/StorageDropboxTest.java
@@ -0,0 +1,22 @@
+package storage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by wei-qiang on 31-May-17.
+ */
+class StorageDropboxTest {
+    @org.junit.jupiter.api.Test
+    void getCurrentDirectory() {
+        StorageDropbox storage = new StorageDropbox();
+        assertEquals("/", storage.getCurrentDirectory());
+    }
+
+    @org.junit.jupiter.api.Test
+    void setCurrentDirectory() {
+        StorageDropbox storage = new StorageDropbox();
+        assertEquals("/", storage.getCurrentDirectory());
+        storage.setCurrentDirectory("/test");
+        assertEquals("/test", storage.getCurrentDirectory());
+    }
+}
